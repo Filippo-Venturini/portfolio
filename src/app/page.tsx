21ed0f222f16be022c5ca6653efc3212494e7ec0
Filippo Venturini <filippo.venturini8@studio.unibo.it>
2024-11-20 12:57:34 +0100

    feat: responsive grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -124,9 +124,9 @@ export default function Home() {
                                     className="rounded-lg object-cover mr-4 border border-gray-300"
                                 />
                                 <div className="flex flex-col justify-between flex-grow">
-                                    <div className="flex justify-between items-center">
-                                        <h3 className="text-xs md:text-sm md:text-sm text-left font-semibold">{experience.title}</h3>
-                                        <p className="text-xs md:text-sm text-gray-500">{experience.dates}</p>
+                                    <div className="flex justify-between items-center w-full">
+                                        <h3 className="text-xs md:text-sm text-left flex-1 font-semibold">{experience.title}</h3>
+                                        <p className="text-xs md:text-sm text-right text-gray-500">{experience.dates}</p>
                                     </div>
                                     <div className="flex justify-start">
                                         <h4 className="text-xs text-gray-600">{experience.position}</h4>
@@ -156,7 +156,7 @@ export default function Home() {
                                         <p className="text-xs md:text-sm text-gray-500">{education.dates}</p>
                                     </div>
                                     <div className="flex justify-start">
-                                        <h4 className="text-xs text-gray-600">{education.position}</h4>
+                                        <h4 className="text-xs text-left text-gray-600">{education.position}</h4>
                                     </div>
                                     <p className="text-xs md:text-sm text-gray-700 text-left">{education.description}</p>
                                 </div>
@@ -168,13 +168,13 @@ export default function Home() {
 
             <section className="max-w-xl w-full mt-12">
                 <h2 className="text-2xl font-semibold text-center mb-4 text-start">Personal Projects</h2>
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-5">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
                     {projects.map((project, index) => (
                         <a key={index} href={project.link} target="_blank" rel="noopener noreferrer" className="block">
                             <Card
                                 style={{ padding: '16px', maxWidth: '270px', willChange: 'transform' }}
-                                className={`glassmorphic py-4 rounded-xl w-full transition-transform transform hover:scale-105 ${
-                                    index % 2 === 0 ? 'justify-self-end' : 'justify-self-start'
+                                className={`glassmorphic py-4 rounded-xl w-full transition-transform transform hover:scale-105 justify-self-center ${
+                                    index % 2 === 0 ? 'md:justify-self-end' : 'md:justify-self-start'
                                 }`}
                             >
                                 <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
