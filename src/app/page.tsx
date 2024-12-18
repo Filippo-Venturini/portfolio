f26103e01a0429dfbc18fa37dbe06052d157bf1c
Filippo Venturini <filippo.venturini8@studio.unibo.it>
2024-12-18 09:55:47 +0100

    feat: force light theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import {Card, CardHeader, CardBody} from "@nextui-org/card";
 import {FaHome, FaLinkedin, FaGithub, FaSun} from "react-icons/fa";
 import Head from "next/head";
+import PageTheme from "@/app/page_theme";
 
 export default function Home() {
     const experiences = [
@@ -90,127 +91,129 @@ export default function Home() {
     ];
 
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen p-8 gap-8 md:p-20 font-[family-name:var(--font-geist-sans)]">
-            <Head>
-                <title>Filippo Venturini</title>
-            </Head>
-            <section className="max-w-xl">
-                <div className="flex items-center gap-4">
-                    <div>
-                        <h1 className="text-3xl md:text-6xl font-bold">Hi, I&#39;m Filippo</h1>
-                        <p className="text-base md:text-lg">Software Engineer, passionate about IoT, Machine Learning, Distributed Systems, and VR/AR systems. 🚀🤖✨</p>
+        <PageTheme>
+            <div className="flex flex-col items-center justify-center min-h-screen p-8 gap-8 md:p-20 font-[family-name:var(--font-geist-sans)]">
+                <Head>
+                    <title>Filippo Venturini</title>
+                </Head>
+                <section className="max-w-xl">
+                    <div className="flex items-center gap-4">
+                        <div>
+                            <h1 className="text-3xl md:text-6xl font-bold">Hi, I&#39;m Filippo</h1>
+                            <p className="text-base md:text-lg">Software Engineer, passionate about IoT, Machine Learning, Distributed Systems, and VR/AR systems. 🚀🤖✨</p>
+                        </div>
+
+                        <Image
+                            src="/profile_photo.png"
+                            alt="Foto di Filippo"
+                            width={150}
+                            height={150}
+                            className="rounded-full object-cover"
+                            priority
+                        />
                     </div>
+                </section>
 
-                    <Image
-                        src="/profile_photo.png"
-                        alt="Foto di Filippo"
-                        width={150}
-                        height={150}
-                        className="rounded-full object-cover"
-                        priority
-                    />
+                <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 z-50 flex items-center justify-around bg-white/80 backdrop-blur-md rounded-full shadow-md p-4 w-full max-w-64 md:max-w-80">
+                    <a><FaHome className="text-xl cursor-pointer hover:text-blue-500 transition" /></a>
+                    <a href="https://www.linkedin.com/in/filippo-venturini-4265b8235/" target="_blank"><FaLinkedin className="text-xl cursor-pointer hover:text-blue-500 transition" /></a>
+                    <a href="https://github.com/Filippo-Venturini" target="_blank"><FaGithub className="text-xl cursor-pointer hover:text-blue-500 transition" /></a>
+                    <FaSun className="text-xl cursor-pointer hover:text-blue-500 transition"></FaSun>
                 </div>
-            </section>
 
-            <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 z-50 flex items-center justify-around bg-white/80 backdrop-blur-md rounded-full shadow-md p-4 w-full max-w-64 md:max-w-80">
-                <a><FaHome className="text-xl cursor-pointer hover:text-blue-500 transition" /></a>
-                <a href="https://www.linkedin.com/in/filippo-venturini-4265b8235/" target="_blank"><FaLinkedin className="text-xl cursor-pointer hover:text-blue-500 transition" /></a>
-                <a href="https://github.com/Filippo-Venturini" target="_blank"><FaGithub className="text-xl cursor-pointer hover:text-blue-500 transition" /></a>
-                <FaSun className="text-xl cursor-pointer hover:text-blue-500 transition"></FaSun>
-            </div>
-
-            <section className="text-center max-w-xl">
-                <h2 className="text-xl font-semibold text-start">About</h2>
-                <p className="mb-4 text-md text-gray-600 text-start">
-                    Hi! I&#39;m Filippo, I&#39;m currently working as a Software Engineer at Blue Reply IT.
-                </p>
-                <h2 className="text-xl font-semibold mt-4 mb-2 text-start">Work Experience</h2>
-                <ul className="list-none space-y-8">
-                    {experiences.map((experience, index) => (
-                        <a key={index} href={experience.link} target="_blank" className="flex items-start space-x-4 hover:bg-gray-100 p-4 rounded-lg transition">
-                            <li className="flex items-start space-x-4">
-                                <Image
-                                    src={experience.logo}
-                                    alt={`Logo di ${experience.title}`}
-                                    width={50}
-                                    height={50}
-                                    className="rounded-lg object-cover mr-4 border border-gray-300"
-                                />
-                                <div className="flex flex-col justify-between flex-grow">
-                                    <div className="flex justify-between items-center w-full">
-                                        <h3 className="text-xs md:text-sm text-left flex-1 font-semibold">{experience.title}</h3>
-                                        <p className="text-xs md:text-sm text-right text-gray-500">{experience.dates}</p>
-                                    </div>
-                                    <div className="flex justify-start">
-                                        <h4 className="text-xs text-gray-600">{experience.position}</h4>
-                                    </div>
-                                    <p className="text-xs md:text-sm text-gray-700 text-left">{experience.description}</p>
-                                </div>
-                            </li>
-                        </a>
-                    ))}
-                </ul>
-
-                <h2 className="text-xl font-semibold mt-4 mb-2 text-start">Education</h2>
-                <ul className="list-none space-y-8">
-                    {educations.map((education, index) => (
-                        <a key={index} href={education.link} target="_blank" className="flex items-start space-x-4 hover:bg-gray-100 p-4 rounded-lg transition">
-                            <li className="flex items-start space-x-4">
-                                <Image
-                                    src={education.logo}
-                                    alt={`Logo di ${education.title}`}
-                                    width={50}
-                                    height={50}
-                                    className="rounded-lg object-cover mr-4 border border-gray-300"
-                                />
-                                <div className="flex flex-col justify-between flex-grow">
-                                    <div className="flex justify-between items-center">
-                                        <h3 className="text-xs md:text-sm text-lg text-left font-semibold">{education.title}</h3>
-                                        <p className="text-xs md:text-sm text-gray-500">{education.dates}</p>
-                                    </div>
-                                    <div className="flex justify-start">
-                                        <h4 className="text-xs text-left text-gray-600">{education.position}</h4>
-                                    </div>
-                                    <p className="text-xs md:text-sm text-gray-700 text-left">{education.description}</p>
-                                </div>
-                            </li>
-                        </a>
-                    ))}
-                </ul>
-            </section>
-
-            <section className="max-w-xl w-full mt-12">
-                <h2 className="text-2xl font-semibold text-center mb-4 text-start">Personal Projects</h2>
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-                    {projects.map((project, index) => (
-                        <a key={index} href={project.link} target="_blank" rel="noopener noreferrer" className="block">
-                            <Card
-                                style={{ padding: '16px', maxWidth: '270px', willChange: 'transform' }}
-                                className={`glassmorphic py-4 rounded-xl w-full transition-transform transform hover:scale-105 justify-self-center ${
-                                    index % 2 === 0 ? 'md:justify-self-end' : 'md:justify-self-start'
-                                }`}
-                            >
-                                <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-                                    <p className="text-tiny uppercase font-bold">{project.title}</p>
-                                    <p className="text-tiny">{project.description}</p>
-                                    <small className="text-default-500">{project.technologies}</small>
-                                </CardHeader>
-                                <CardBody className="overflow-visible py-2">
+                <section className="text-center max-w-xl">
+                    <h2 className="text-xl font-semibold text-start">About</h2>
+                    <p className="mb-4 text-md text-gray-600 text-start">
+                        Hi! I&#39;m Filippo, I&#39;m currently working as a Software Engineer at Blue Reply IT.
+                    </p>
+                    <h2 className="text-xl font-semibold mt-4 mb-2 text-start">Work Experience</h2>
+                    <ul className="list-none space-y-8">
+                        {experiences.map((experience, index) => (
+                            <a key={index} href={experience.link} target="_blank" className="flex items-start space-x-4 hover:bg-gray-100 p-4 rounded-lg transition">
+                                <li className="flex items-start space-x-4">
                                     <Image
-                                        alt="Card background"
-                                        className="object-cover rounded-xl"
-                                        src={project.image}
-                                        width={270}
-                                        height={270}
+                                        src={experience.logo}
+                                        alt={`Logo di ${experience.title}`}
+                                        width={50}
+                                        height={50}
+                                        className="rounded-lg object-cover mr-4 border border-gray-300"
                                     />
-                                </CardBody>
-                            </Card>
-                        </a>
+                                    <div className="flex flex-col justify-between flex-grow">
+                                        <div className="flex justify-between items-center w-full">
+                                            <h3 className="text-xs md:text-sm text-left flex-1 font-semibold">{experience.title}</h3>
+                                            <p className="text-xs md:text-sm text-right text-gray-500">{experience.dates}</p>
+                                        </div>
+                                        <div className="flex justify-start">
+                                            <h4 className="text-xs text-gray-600">{experience.position}</h4>
+                                        </div>
+                                        <p className="text-xs md:text-sm text-gray-700 text-left">{experience.description}</p>
+                                    </div>
+                                </li>
+                            </a>
+                        ))}
+                    </ul>
 
-                    ))}
-                </div>
-            </section>
+                    <h2 className="text-xl font-semibold mt-4 mb-2 text-start">Education</h2>
+                    <ul className="list-none space-y-8">
+                        {educations.map((education, index) => (
+                            <a key={index} href={education.link} target="_blank" className="flex items-start space-x-4 hover:bg-gray-100 p-4 rounded-lg transition">
+                                <li className="flex items-start space-x-4">
+                                    <Image
+                                        src={education.logo}
+                                        alt={`Logo di ${education.title}`}
+                                        width={50}
+                                        height={50}
+                                        className="rounded-lg object-cover mr-4 border border-gray-300"
+                                    />
+                                    <div className="flex flex-col justify-between flex-grow">
+                                        <div className="flex justify-between items-center">
+                                            <h3 className="text-xs md:text-sm text-lg text-left font-semibold">{education.title}</h3>
+                                            <p className="text-xs md:text-sm text-gray-500">{education.dates}</p>
+                                        </div>
+                                        <div className="flex justify-start">
+                                            <h4 className="text-xs text-left text-gray-600">{education.position}</h4>
+                                        </div>
+                                        <p className="text-xs md:text-sm text-gray-700 text-left">{education.description}</p>
+                                    </div>
+                                </li>
+                            </a>
+                        ))}
+                    </ul>
+                </section>
 
-        </div>
-    );
+                <section className="max-w-xl w-full mt-12">
+                    <h2 className="text-2xl font-semibold text-center mb-4 text-start">Personal Projects</h2>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+                        {projects.map((project, index) => (
+                            <a key={index} href={project.link} target="_blank" rel="noopener noreferrer" className="block">
+                                <Card
+                                    style={{ padding: '16px', maxWidth: '270px', willChange: 'transform' }}
+                                    className={`glassmorphic py-4 rounded-xl w-full transition-transform transform hover:scale-105 justify-self-center ${
+                                        index % 2 === 0 ? 'md:justify-self-end' : 'md:justify-self-start'
+                                    }`}
+                                >
+                                    <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+                                        <p className="text-tiny uppercase font-bold">{project.title}</p>
+                                        <p className="text-tiny">{project.description}</p>
+                                        <small className="text-default-500">{project.technologies}</small>
+                                    </CardHeader>
+                                    <CardBody className="overflow-visible py-2">
+                                        <Image
+                                            alt="Card background"
+                                            className="object-cover rounded-xl"
+                                            src={project.image}
+                                            width={270}
+                                            height={270}
+                                        />
+                                    </CardBody>
+                                </Card>
+                            </a>
+
+                        ))}
+                    </div>
+                </section>
+
+            </div>
+        </PageTheme>);
+
 }
