8f16fe7aaca47b552e419ce365f06ef66b060162
Filippo Venturini <filippoventurini00@gmail.com>
2026-02-23 15:49:36 +0100

    fix(css): add external provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,29 @@
-"use client";
-
 import localFont from "next/font/local";
 import "@/styles/globals.css";
-import { NextUIProvider } from "@nextui-org/react"; 
+import { Providers } from "./providers";
 
 const geistSans = localFont({
-    src: "../../public/fonts/GeistVF.woff", 
-    variable: "--font-geist-sans",
-    weight: "100 900",
+  src: "../../public/fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
 });
 
 const geistMono = localFont({
-    src: "../../public/fonts/GeistMonoVF.woff", 
-    variable: "--font-geist-mono",
-    weight: "100 900",
+  src: "../../public/fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
 });
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: React.ReactNode;
+  children,
+}: Readonly<{
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-        <body
-            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-        <NextUIProvider>
-            {children}
-        </NextUIProvider>
-        </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
 }
